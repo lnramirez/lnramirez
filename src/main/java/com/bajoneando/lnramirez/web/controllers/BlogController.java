116430c1a69129581695a240c671cdb330e1758f
Luis Ramirez <lnramirez@gmail.com>
2012-05-20 15:00:16 -0400

    fixing imports

--- src/main/java/com/bajoneando/lnramirez/web/controllers/BlogController.java
+++ src/main/java/com/bajoneando/lnramirez/web/controllers/BlogController.java
@@ -4,7 +4,6 @@ import com.bajoneando.lnramirez.blog.BlogEntry;
 import com.bajoneando.lnramirez.blog.services.BlogEntryRepository;
 import com.petebevin.markdown.MarkdownProcessor;
 import java.util.Date;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
