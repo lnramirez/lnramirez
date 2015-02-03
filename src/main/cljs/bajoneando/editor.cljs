d4115d809812283781a6f1d2b4b29033858be45d
Luis Ramirez Monterosa <luis@brazencareerist.com>
2015-02-03 18:09:44 -0500

    navigation functionality works, except click is not preventable 'yet'

--- src/main/cljs/bajoneando/editor.cljs
+++ src/main/cljs/bajoneando/editor.cljs
@@ -1,5 +1,6 @@
 (ns bajoneando.editor
   (:require-macros [cljs.core.async.macros :refer [go]])
+  (:import [goog.i18n DateTimeFormat])
   (:require [cljs.core.async :refer [put! <! chan]]
             [cognitect.transit :as transit]
             [om.core :as om :include-macros true]
@@ -20,6 +21,11 @@
 
 (def app-state (atom {:page 1}))
 
+(def date-formatter (DateTimeFormat. "dd-MMM-yyyy"))
+(def in-date-formatter (DateTimeFormat. "yyyy-MM-dd"))
+(def date-time-formatter (DateTimeFormat. "dd-MMM-yyyy HH:mm"))
+(def in-date-time-formatter (DateTimeFormat. "yyyy-MM-dd HH:mm"))
+
 (defn nav-pages [app owner op]
       (let [entries (om/get-state owner [:entries-chan])
             page (op (:page @app))]
@@ -37,6 +43,8 @@
           [_]
           {:entries []
            :entries-chan  (chan)
+           :first-page true
+           :last-page false
            :page 1})
         om/IWillMount
         (will-mount
@@ -44,7 +52,10 @@
           (let [entries (om/get-state owner [:entries-chan])]
                (go (while true
                           (let [res (<! entries)]
-                               (om/set-state! owner :entries (get res "content"))
+                               (do
+                                 (om/set-state! owner :entries (get res "content"))
+                                 (om/set-state! owner :first-page (get res "firstPage"))
+                                 (om/set-state! owner :last-page (get res "lastPage")))
                                )))
                (bcore/js-xhr {:method :get
                               :url (str "/blog/entries?page.page=" (:page app))
@@ -54,7 +65,9 @@
         (render-state
           [this state]
           (let [entries (:entries state)
-                page (:page state)]
+                page (:page state)
+                first-page (:first-page state)
+                last-page (:last-page state)]
                (html [:div
                       (for [entry entries]
                            [:article
@@ -66,25 +79,51 @@
                                (get entry "subject")]]
                              [:p.text-muted
                               (str "Published on: ")
-                              [:time.publishDate
-                               {:dateTime (get entry "publishDate")
-                                :format "yyyy-mm-dd"}
-                               (get entry "publishDate")]]]
+                              (let [in-date (js/Date. (get entry "publishDate"))
+                                    publish-date (.format date-formatter in-date)
+                                    in-publish-date (.format in-date-formatter in-date)]
+                                   [:time.publishDate
+                                    {:dateTime in-publish-date}
+                                    publish-date])
+                              ]]
                             [:div.printableHtml
                              {:dangerouslySetInnerHTML #js {:__html (get entry "printableHtml")}}
-                             ]])
-                      [:ul.pager
-                       [:li.previous
-                        [:a
-                         {:on-click #(nav-pages app owner inc)}
-                         "Previous Entries"]
-                        ]
-                       [:li.next
-                        [:a
-                         {:on-click #(nav-pages app owner dec)}
-                         "Later Entries"]]]
-                      ])
-               ))))
+                             ]
+                            (let [in-date (js/Date. (get entry "lastUpdateDate"))
+                                  last-updated (.format date-time-formatter in-date)
+                                  in-last-updated (.format in-date-time-formatter in-date)]
+                                 [:p.text-muted
+                                  "Last update: "
+                                  [:time
+                                   {:dateTime in-last-updated}
+                                   last-updated]])
+                            ])
+
+                      [:nav
+                       [:ul.pager
+                        [:li.previous
+                         {:class (if last-page
+                                   "disabled")}
+                         [:a
+                          {:href "#"
+                           :on-click #(nav-pages app owner inc)}
+                          [:span
+                           {:aria-hidden true}
+                           "←"]
+                          "Older"]]
+                        [:li.next
+                         {:class (if first-page
+                                   "disabled")}
+                         [:a
+                          {:href "#"
+                           :on-click #(nav-pages app owner dec)}
+                          "Newer"
+                          [:span
+                           {:aria-hidden true}
+                           "→"]]
+                         ]]]
+
+                      ])))))
 
 (om/root
   articles-render
